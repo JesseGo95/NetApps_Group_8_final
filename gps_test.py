52b6e3d24b4e6a1e151d302033bdee55ce6d2eac
JesseGo95 <jesse95@vt.edu>
2017-05-09 06:16:21 +0000

    updated to send data json though xbee; also updated calculation for lat and long

--- gps_test.py
+++ gps_test.py
@@ -1,8 +1,17 @@
 import serial
 import json
 import time
+from datetime import datetime
+#from pytz import timezone
+
+#tz = timezone('EST')
 
 port = serial.Serial("/dev/ttyUSB0", 9600)
+xbee = serial.Serial("/dev/ttyUSB1", 9600)
+
+data = {"time":0}
+data["lat"]=0
+data["long"]=-0
 
 port.write("$PMTK220,5000*1B\r\n".encode())
 port.write("$PMTK300,5000,0,0,0,0*18\r\n".encode())
@@ -10,12 +19,32 @@ port.write("$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29\r\n".encode())
 while True:
     x = port.readline()
     x = x.decode().strip()
+    line = x.split(",")
+    data["time"] = time.strftime("%b %d %Y %H:%M:%S", datetime.utcnow().timetuple())
+    try:
+        lat = float(line[3])/100
+        lon = float(line[5])/100
+    except:
+        print ("ERROR: invalid data")
+        string = json.dumps(data)+"\n"
+        xbee.write(string.encode())
+        print(data)
+        continue
+    if line[4] == "S":
+        lat = lat * -1
+    if line[6] == "W":
+        lon = lon * -1
+    data["lat"] = lat
+    data["long"] = lon
+    print(data)
+    string = json.dumps(data)+"\n"
+    xbee.write(string.encode())
     #data = json.loads(x.decode())
     #print(x)
-    try:
-        time.sleep(1)
+    #try:
+    #    time.sleep(1)
         #data = json.loads(x)
-        print(x)
-    except:
-        print("Cannot parse data")
+    #    print(x)
+    #except:
+    #    print("Cannot parse data")
 
